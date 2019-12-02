529d52f9c0e4af1d0c291a14fd864d0c8738ce2d
Сергей Емельянов <SYemelianau@gomel.iba.by>
2019-12-02 15:01:15 +0300

    jses5 - task1
    Change-Id: I7ca1186f657b1399f4f9b44ea7af498518957836

--- src/Task1/task1.js
+++ src/Task1/task1.js
@@ -1,9 +1,8 @@
 function forLoop() {
     var answer = '';
-//cycle will return "#\n##\n###\n####\n#####\n######\n#######\n" string
+    // loop, that create '#\n##\n###\n####\n#####\n######\n#######\n'
     for (var string = '#'; string.length < 8; string += '#') {
         answer += (string + '\n');
     }
-
     return answer;
 }
